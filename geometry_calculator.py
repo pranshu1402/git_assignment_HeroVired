b6e852b0068891ce6ed452d0a655b4a238001f09
pranshu1402 <pranshu.ghps@gmail.com>
2025-05-04 23:41:26 +0530

    Implemented circle area

--- geometry_calculator.py
+++ geometry_calculator.py
@@ -3,6 +3,10 @@ import math
 class GeometryCalculator:
     def calculate_rectangle_area(self, length, width):
         return length * width
+    
+    def calculate_circle_area(self, radius):
+        return math.pi * (radius ** 2)
+
 
 if __name__ == "__main__":
     calculator = GeometryCalculator()
@@ -12,3 +16,7 @@ if __name__ == "__main__":
     length = int(length)
     width = int(width)
     print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
+
+    radius = input("Enter the radius of the circle: ")
+    radius = int(radius)
+    print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
